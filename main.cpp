cfa219160961b81c01edb5939426f0c61bacec23
Chip Brommer <chipbrommer@gmail.com>
2024-02-27 22:28:23 -0600

    add abilities for objects and arrays.

--- main.cpp
+++ main.cpp
@@ -14,8 +14,21 @@
 //
 /////////////////////////////////////////////////////////////////////////////////
 
-// Function to load JSON file
-nlohmann::json LoadJsonFile(const std::string& filename) 
+constexpr int MAJOR_VERSION = 0;
+constexpr int MINOR_VERSION = 0;
+constexpr int BUILD_VERSION = 1;
+
+static bool g_exitRequested = false;
+
+/// @brief Function declaration for UpdateJsonArray
+/// @param array - json array
+/// @param arrayName - name of the key for the array
+static void UpdateJsonArray(nlohmann::json& array, const std::string& arrayName);
+
+/// @brief Load the json file into a json object
+/// @param filename - filename/path to be loaded
+/// @return - json object of the file contents
+static nlohmann::json LoadJsonFile(const std::string& filename)
 {
     std::ifstream file(filename);
     nlohmann::json jsonData;
@@ -23,8 +36,10 @@ nlohmann::json LoadJsonFile(const std::string& filename)
     return jsonData;
 }
 
-// Function to validate boolean input
-bool ValidateBooleanInput(const std::string& input) 
+/// @brief Function to validate boolean input
+/// @param input - checks string input for valid mapping to true/false
+/// @return - true if the string is a bool value "true" or "false", false if not. 
+static bool ValidateBooleanInput(const std::string& input)
 {
     std::string lowercaseInput;
     for (char c : input) 
@@ -34,16 +49,189 @@ bool ValidateBooleanInput(const std::string& input)
     return lowercaseInput == "true" || lowercaseInput == "false";
 }
 
+/// @brief Function to update JSON object
+/// @param object - json object
+/// @param objectName - optional - name of the object key
+static void UpdateJsonObject(nlohmann::json& object, const std::string& objectName = "")
+{
+    for (auto it = object.begin(); it != object.end(); ++it)
+    {
+        // Return if exit requested - At top for recursive catch
+        if (g_exitRequested)
+        {
+            return;
+        }
+
+        // Get the key name and determine the type of the value
+        std::string key = it.key();
+        std::string valueType;
+
+        if (it.value().is_string()) { valueType = "string"; }
+        else if (it.value().is_number_integer()) { valueType = "integer"; }
+        else if (it.value().is_number_float()) { valueType = "double"; }
+        else if (it.value().is_boolean()) { valueType = "boolean"; }
+        else if (it.value().is_array()) { 
+            UpdateJsonArray(object[key], key); continue; }
+        else if (it.value().is_object()) { UpdateJsonObject(object[key], key); continue; } // Recursively update the object
+        else if (it.value().is_null()) { continue; }
+        else { continue; }
+
+        // Display info to user for item they're updating
+        if (objectName == "")
+            std::cout << "Enter value for " << key << " (" << valueType << "): ";
+        else
+            std::cout << "Enter value for " << key << " within <" << objectName << "> (" << valueType << "): ";
+
+        while (!g_exitRequested)
+        {
+            // Get the input
+            std::string input;
+            std::cin >> input;
+
+            // Check for accepted flags
+            if (input == "-n") { break; }
+            if (input == "-x") { g_exitRequested = true; break; }
+
+            // Parse input based on the object type. 
+            if (valueType == "string")
+            {
+                object[key] = input;
+                break;
+            }
+            else if (valueType == "integer")
+            {
+                try
+                {
+                    int value = std::stoi(input);
+                    object[key] = value;
+                    break;
+                }
+                catch (const std::invalid_argument& e)
+                {
+                    std::cout << "Invalid input. Please enter an integer value." << std::endl;
+                }
+            }
+            else if (valueType == "double")
+            {
+                try
+                {
+                    double value = std::stod(input);
+                    object[key] = value;
+                    break;
+                }
+                catch (const std::invalid_argument& e)
+                {
+                    std::cout << "Invalid input. Please enter a double value." << std::endl;
+                }
+            }
+            else if (valueType == "boolean")
+            {
+                if (input == "true" || input == "false")
+                {
+                    object[key] = (input == "true");
+                    break;
+                }
+                else
+                {
+                    std::cout << "Invalid input for boolean. Please enter 'true' or 'false'." << std::endl;
+                }
+            }
+        }
+    }
+}
+
+/// @brief Function declaration for UpdateJsonArray
+/// @param array - json array
+/// @param arrayName - name of the key for the array
+static void UpdateJsonArray(nlohmann::json& array, const std::string& arrayName)
+{
+    for (size_t i = 0; i < array.size(); ++i) 
+    {
+        std::string valueType;
+        if (array[i].is_string())               { valueType = "string";     }
+        else if (array[i].is_number_integer())  { valueType = "integer";    }
+        else if (array[i].is_number_float())    { valueType = "double";     }
+        else if (array[i].is_boolean())         { valueType = "boolean";    }
+        else if (array[i].is_array())           { UpdateJsonArray(array[i], arrayName);  continue;   }
+        else if (array[i].is_object())          { std::string name = arrayName + "[" + std::to_string(i) + "]"; UpdateJsonObject(array[i], name); continue; }
+        else if (array[i].is_null())            { continue; }
+        else { continue;}
+
+        std::cout << "Enter value for element " << i << " in <" << arrayName << "> (" << valueType << "): ";
+
+        while (!g_exitRequested) 
+        {
+            std::string input;
+            std::cin >> input;
+
+            if (input == "-n") { break; }
+            if (input == "-x") { g_exitRequested = true; break; }
+
+            if (valueType == "string") 
+            {
+                array[i] = input;
+                break;
+            }
+            else if (valueType == "integer") 
+            {
+                try 
+                {
+                    int value = std::stoi(input);
+                    array[i] = value;
+                    break;
+                }
+                catch (const std::invalid_argument& e) 
+                {
+                    std::cout << "Invalid input. Please enter an integer value." << std::endl;
+                }
+            }
+            else if (valueType == "double") 
+            {
+                try 
+                {
+                    double value = std::stod(input);
+                    array[i] = value;
+                    break;
+                }
+                catch (const std::invalid_argument& e) 
+                {
+                    std::cout << "Invalid input. Please enter a double value." << std::endl;
+                }
+            }
+            else if (valueType == "boolean") 
+            {
+                if (input == "true" || input == "false") 
+                {
+                    array[i] = (input == "true");
+                    break;
+                }
+                else 
+                {
+                    std::cout << "Invalid input for boolean. Please enter 'true' or 'false'." << std::endl;
+                }
+            }
+        }
+    }
+}
+
+/// @brief main working function. Program entry point. 
+/// @return 1
 int main() 
 {
-    bool exitRequsted = false;
-
+    // Hard coded file paths for now.
     std::vector<std::string> fileList = { 
         "C:\\Users\\chipb\\Desktop\\updater_settings.json", 
         "C:\\Users\\chipb\\Desktop\\updater_settings.json", 
         "C:\\Users\\chipb\\Desktop\\updater_settings.json" 
     };
 
+    // Display title 
+    std::string title = "\n=========================\n";
+    title            += "   Config File Updater   \n";
+    title            += "=========================\n\n";
+    std::cout << title;
+
+    // List the available files
     std::cout << "Available files:" << '\n';
     int index = 1;
     for (const auto& file : fileList) 
@@ -51,14 +239,16 @@ int main()
         std::cout << "\t[" << index++ << "] " << file << std::endl;
     }
 
+    // Capture file selection and make sure its within bounds.
+    // accepts '-x' to close the program. 
     std::string textFileIndex;
-    int fileIndex;
-    while (!exitRequsted)
+    int fileIndex = 0;
+    while (!g_exitRequested)
     {
         std::cout << "Enter the number of the file you want to load: ";
         std::cin >> textFileIndex;
 
-        if (textFileIndex == "-x") { exitRequsted = true; break; }
+        if (textFileIndex == "-x") { g_exitRequested = true; break; }
 
         // Validate user input
         try {
@@ -82,136 +272,16 @@ int main()
         
     }
 
+    // Notify the selection and available flags. 
     std::string selectedFile = fileList[fileIndex - 1];
     std::cout << "You selected: " << selectedFile << '\n';
+    std::cout << "\nEnter '-n' to skip any item.\nEnter '-x' to exit the update and save any edits.\n\n";
 
     // Load JSON file
     nlohmann::json configFile = LoadJsonFile(selectedFile);
 
-    // Prompt user for input for each key
-    for (auto it = configFile.begin(); it != configFile.end(); ++it) 
-    {
-        std::string key = it.key();
-        std::string valueType;
-
-        // Determine the type of the value
-        if (it.value().is_string())                 { valueType = "string";     }
-        else if (it.value().is_number_integer())    { valueType = "integer";    }
-        else if (it.value().is_number_float())      { valueType = "double";     }
-        else if (it.value().is_boolean())           { valueType = "boolean";    }
-        else if (it.value().is_array())             { valueType = "array";      }
-        else if (it.value().is_object())            { valueType = "object";     }
-        else if (it.value().is_null())              { valueType = "null";       }
-        else                                        { valueType = "unknown";    }
-
-        std::cout << "Enter value for " << key << " (" << valueType << "): ";
-
-        //while (true)
-        //{
-        //    if (valueType == "string") 
-        //    {
-        //        std::string value;
-        //        std::cin >> value;
-        //        configFile[key] = value;
-        //        break;
-        //    }
-        //    else if (valueType == "integer") 
-        //    {
-        //        int value;
-        //        std::cin >> value;
-        //        configFile[key] = value;
-        //        break;
-        //    }
-        //    else if (valueType == "double") 
-        //    {
-        //        double value;
-        //        std::cin >> value;
-        //        configFile[key] = value;
-        //        break;
-        //    }
-        //    else if (valueType == "boolean") 
-        //    {
-        //        std::string value;
-        //        std::cin >> value;
-
-        //        if (ValidateBooleanInput(value)) 
-        //        {
-        //            configFile[key] = (value == "true");
-        //            break;
-        //        }
-        //        else 
-        //        {
-        //            std::cout << "Invalid input for boolean. Please enter 'true' or 'false'." << '\n';
-        //        }
-        //    }
-        //    
-        //}
-    
-        // Repeat until valid input is provided
-        while (!exitRequsted) {
-            std::string input;
-            std::cin >> input;
-
-            // Check for flags
-            if (input == "-n") { break; }
-            if (input == "-x") { exitRequsted = true; break; }
-
-            // Check input
-            if (valueType == "string") 
-            {
-                configFile[key] = input;
-                break;
-            }
-            else if (valueType == "integer") 
-            {
-                int value;
-                try 
-                {
-                    value = stoi(input);
-                    configFile[key] = value;
-                    break;
-                }
-                catch (const std::invalid_argument& e) 
-                {
-                    std::cout << "Invalid input. Please enter an integer value." << '\n';
-                }
-            }
-            else if (valueType == "double") 
-            {
-                double value;
-                try 
-                {
-                    value = stod(input);
-                    configFile[key] = value;
-                    break;
-                }
-                catch (const std::invalid_argument& e) 
-                {
-                    std::cout << "Invalid input. Please enter a double value." << '\n';
-                }
-            }
-            else if (valueType == "boolean") 
-            {
-                if (ValidateBooleanInput(input)) 
-                {
-                    configFile[key] = (input == "true");
-                    break; // Exit loop if input is valid
-                }
-                else 
-                {
-                    std::cout << "Invalid input for boolean. Please enter 'true' or 'false'." << '\n';
-                    std::cout << "Enter value for " << key << " (" << valueType << ") or enter \"-n\" to skip: ";
-                }
-            }
-            else if (valueType == "array" || valueType == "object" || valueType == "null") {
-                std::cout << "Invalid value type for this input. Please enter a different value or enter \"-n\" to skip." << '\n';
-                std::cout << "Enter value for " << key << " (" << valueType << ") or enter \"-n\" to skip: ";
-            }
-        }
-    
-        // Break out of for loop
-        if (exitRequsted) { break; }
-    }
+    // Call update function
+    UpdateJsonObject(configFile);
 
     // Save updated JSON to file
     std::ofstream outputFile(selectedFile);
